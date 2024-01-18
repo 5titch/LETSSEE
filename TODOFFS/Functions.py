00a8010d93454ef53b75ecc9bc04a0e2e659a5e9
Ardit <samuel7mensah@gmail.com>
2024-01-18 18:38:34 +0000

    Initial commit

---
+++ TODOFFS/Functions.py
@@ -0,0 +1,18 @@
+FILEPATH = "todos3.txt"
+def get_t(filepath=FILEPATH):
+    '''opens and reads the filepath in this case todos2 and thes stores in varianble in this
+    case file_l. then stores in todos_l then returns the content
+    '''
+    with open(filepath, 'r') as file_l:
+        todos_l = file_l.readlines()
+    return todos_l
+
+
+def write_t(todos_a, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_a)
+
+
+print(f'{__name__} Has been imported')
+if __name__ == '__main__':
+    print('running directly from functions ')
---
+++ TODOFFS/WEB.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import Functions
+
+todos = Functions.get_t()
+
+
+def add_todo():
+    todo = st.session_state["IT"] + '\n'
+    todos.append(todo)
+    Functions.write_t(todos)
+
+st.title("To Do list")
+st.subheader("Pattern your life")
+st.write("This app is created to better your life")
+
+
+
+for i, todo in enumerate(todos):                   # for every item in toodo enumerate it and...
+    checkbox = st.checkbox(todo, key=todo)      # assign the a checkbox to it then put it in checkbock give it key of toodo
+    if checkbox:                        # if checkbox is true as its default is fau
+        todos.pop(i)                        # this actually removes
+        Functions.write_t(todos)
+        del st.session_state[todo]   # this removes the session state without as without it, it just becomes true
+        st.experimental_rerun()
+
+
+st.text_input(label="", placeholder="Add new task", on_change=add_todo, key="IT" )  # Label is part if this function
+
+print("hello")
+st.session_state
+
+print("yh")
