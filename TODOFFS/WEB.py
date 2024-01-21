4da44f6f5d941838a1ef120cfde2effac943d200
Ardit <samuel7mensah@gmail.com>
2024-01-21 13:28:01 +0000

    more tweaks regarding removing todos n

--- TODOFFS/WEB.py
+++ TODOFFS/WEB.py
@@ -13,24 +13,38 @@ st.title("To Do list")
 st.subheader("Pattern your life")
 st.write("This app is created to better your life")
 
-remove = []
+checkbox_states = st.session_state.get("checkbox_states", {})
 
-if todos is not None:
-    for i, todo in enumerate(todos):
-        checkbox = st.checkbox(todo, key=todo)
-        if checkbox:
-            remove.append(i)
-            st.warning(f"Removing{remove}")
+# if todos is not None:
+#     for i, toodo in enumerate(todos):
+#         checkbox = st.checkbox(toodo, key=todo)
+#         if checkbox:
+#             remove.append(i)
+#             st.warning(f"removing {toodo} and {i}")
+#     if remove:
+#         st.warning(f"Removing{remove}")
+#     for g in reversed(remove):
+#        todos.pop(g)
+
+for i, todo in enumerate(todos):
+    checkbox_states[i] = st.checkbox(todo, key=f"checkbox_{i}", value=checkbox_states.get(i, False))    # get method goes hand in hand with keys
+    print(checkbox_states)
 
 
-    for g in reversed(remove):
-        todos.pop(g)
 
-else:
-    st.warning("issue reading file")
+# removal
+remove_indices = [i for i, state in checkbox_states.items() if state]
+if remove_indices:
+    st.warning(f"{todo}, has been removed")
+    todos = [todo for i, todo in enumerate(todos) if i not in remove_indices]
+    Functions.write_t(todos)
+
+for i in remove_indices:
+    checkbox_states[i] = False
+
+st.session_state.checkbox_states = checkbox_states
 
 st.text_input(label="", placeholder="Add new task", on_change=add_todo, key="IT" )  # Label is part if this function
-
 
 
 
